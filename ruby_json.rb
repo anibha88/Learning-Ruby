08a3ef22f1d92ab75623e063c3b66b801f0adad7
anibha88 <anibha88@gmail.com>
2016-01-11 00:24:20 +0530

    init commit with problem statement

---
+++ ruby_json.rb
@@ -0,0 +1,7 @@
+=begin
+Build a program that:
+1. Reads in user information that is provided.
+2. Reads in permissions from a json file
+3. creates a new user with the assigned permissions.
+4. Saves the user in a new users listing file, if file doesn't exist yet, it creates one
+=end
